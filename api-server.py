e41af6f72e80ede836cb92b2b3072e1731830b35
Neeranjan <hello@neeranjan.com>
2022-06-11 00:07:27 +0800

    Update api-server.py

--- api-server.py
+++ api-server.py
@@ -1,20 +1,10 @@
 from glob import glob
-import time
 import flask
 from flask import Flask, request, redirect, url_for, session, escape, jsonify
 import os, subprocess
-import base64
-import string
 from random import *
 from pip import main
-import requests
-import json
-import random
 import os.path
-import requests 
-import os.path 
-import json 
-import random 
 HOST=""
 USERNAME=""
 PASSWORD=""
@@ -35,6 +25,14 @@ def get_temperature():
     out = subprocess.Popen(f"ipmitool -I lanplus -H {HOST} -U {USERNAME} -P {PASSWORD} sdr type temperature | grep degrees | cut -d ' ' -f 16", shell=True, stdout=subprocess.PIPE).stdout.read()
     return out.decode('utf-8').split("\n")[0]
 
+def set_fan_speed(speed):
+    """Sets the parsed speed as the server''s fan speed"""
+    if speed <= 60 and speed >=0:
+        out = subprocess.Popen(f"ipmitool -I lanplus -H {HOST} -U {USERNAME} -P {PASSWORD} raw 0x30 0x30 0x02 0xff 0x{speed}", shell=True, stdout=subprocess.PIPE).stdout.read()
+        return speed
+    else:
+        return "Please enter a speed inbetween 0 and 60"
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -43,7 +41,12 @@ def home():
 
 @app.route('/get-temperature', methods=['GET'])
 def get_server_temperature():
-    return '''<h1>{}</h1>'''.format(get_temperature())
+    return '''<h1>Current Server Temperature -> {}</h1>'''.format(get_temperature())
+
+
+@app.route('/set-fan-speed/<speed>', methods=['GET'])
+def set_server_fan_speed(speed):
+    return '''<h1>Fan speed has been set to {}</h1>'''.format(set_fan_speed(speed))
 
 
 def main():
